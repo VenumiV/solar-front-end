21999a6f4b266c7f1294068d7985c7c40754c1de
Venumi Alagoda <166162600+VenumiV@users.noreply.github.com>
2025-11-23 14:33:24 +0530

    Added authorization header and routes

--- src/lib/redux/query.js
+++ src/lib/redux/query.js
@@ -5,14 +5,31 @@ const baseUrl = "http://localhost:8000/api";
 // Define a service using a base URL and expected endpoints
 export const api = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: baseUrl }),
+  baseQuery: fetchBaseQuery({ baseUrl: baseUrl,  prepareHeaders: async (headers) => {
+   const clerk = window.Clerk;
+   if (clerk){
+      const token = await clerk.session.getToken();
+      console.log(token);
+    
+    if(token){
+      headers.set("Authorization", `Bearer ${(token)}`)
+    }
+  }
+  
+   return headers;
+ } }),
   endpoints: (build) => ({
     getEnergyGenerationRecordsBySolarUnit: build.query({
       query: ({id, groupBy,limit}) => `/energy-generation-records/solar-unit/${id}?groupBy=${groupBy}&limit=${limit}`,
+    }),
+    getSolarUnitforUser: build.query({
+      query: () => `/solar-units/me`,
+      
+      
     }),
   }),
 });
 
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
-export const { useGetEnergyGenerationRecordsBySolarUnitQuery } = api;
+export const { useGetEnergyGenerationRecordsBySolarUnitQuery, useGetSolarUnitforUserQuery } = api;
--- src/pages/dashboard/dashboard.page.jsx
+++ src/pages/dashboard/dashboard.page.jsx
@@ -1,32 +1,50 @@
-import { useGetEnergyGenerationRecordsBySolarUnitQuery } from "@/lib/redux/query";
+//import { useGetEnergyGenerationRecordsBySolarUnitQuery } from "@/lib/redux/query";
+import { useGetSolarUnitforUserQuery } from "@/lib/redux/query";
 import DataCard from "./components/DataCard";
 import DataChart from "./components/DataChart";
 import { useUser } from "@clerk/clerk-react";
 
 const DashboardPage = () => {
 
-  const { user } = useUser();
-  console.log(user);
-  
-  const solarUnitId = "690c024481b1e60d4e380875";
+  const { user, isLoaded } = useUser(); // from @clerk/clerk-react
 
+const {
+  data: solarUnit,
+  isLoading: isLoadingSolarUnit,
+  isError: isErrorSolarUnit,
+  error: errorSolarUnit,
+} = useGetSolarUnitforUserQuery(undefined, {
+  skip: !user,
+});
+
+
+  if (isLoadingSolarUnit) {
+    return <div>Loading...</div>;
+  }
+
+  if (isErrorSolarUnit) {
+    return <div>Error: {errorSolarUnit.message}</div>;
+  }
+
+  console.log(solarUnit);
+  
   return (
     <main className="mt-4">
       <h1 className="text-4xl font-bold text-foreground">{user?.firstName}'s House</h1>
       <p className="text-gray-600 mt-2">Welcome back to your Solar Energy Production Dashboard</p>
       <div className="mt-8">
         <DataCard
-          solarUnitId={solarUnitId}
+          solarUnitId={solarUnit._Id}
           title="Last 7 Days Energy Production"
         />
       </div>
       <div className="mt-8">
         <DataChart
-         solarUnitId={solarUnitId}
+         solarUnitId={solarUnit._Id}
         />
       </div>
     </main>
   );
-};
+}
 
 export default DashboardPage;
