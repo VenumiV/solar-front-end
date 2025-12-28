dbb6cffc00e9be62a83cb280955f9bc27bbd1c47
Venumi Alagoda <166162600+VenumiV@users.noreply.github.com>
2025-12-28 21:52:06 +0530

    Update import path for Descrip component in home.page.jsx

--- src/pages/home/home.page.jsx
+++ src/pages/home/home.page.jsx
@@ -1,7 +1,7 @@
 import HeroSection from "@/pages/home/components/HeroSection/HeroSection";
 import SolarEnergyProduction from "@/pages/home/components/SolarEnergyProduction";
 import Navigation from "@/components/Navigation/Navigation";
-import Descrip from "@/components/BelowHero/descrip";
+import Descrip from "@/components/BelowHero/Descrip.jsx";
 import Footer from "@/components/Footer/Footer";
 
 
